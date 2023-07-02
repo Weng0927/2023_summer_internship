3684247e0cf304271e4fc7e0ba9eb0516283f119
Weng Jiahao <1010495847@qq.com>
2023-07-02 14:00:19 +0800

    fix: 引入方式更新，bug仍未修复

--- app/konva.js/page.tsx
+++ app/konva.js/page.tsx
@@ -4,41 +4,39 @@ import Konva from "konva";
 import { render } from "react-dom";
 import { Stage, Layer, Rect, Text } from "react-konva";
 
-class ColoredRect extends React.Component {
-    state = {
-        color: "green"
-    };
-    handleClick = () => {
-        this.setState({
-            color: Konva.Util.getRandomColor()
-        });
-    };
-    render() {
-        return (
-            <Rect
-                x={20}
-                y={20}
-                width={50}
-                height={50}
-                fill={this.state.color}
-                shadowBlur={5}
-                onClick={this.handleClick}
-            />
-        );
+function ColoredRect() {
+    const [color, setColor] = React.useState("green");
+
+    const handleClick = () => {
+        setColor(Konva.Util.getRandomColor());
     }
+
+    return (
+        <Rect
+            x={20}
+            y={20}
+            width={50}
+            height={50}
+            fill={color}
+            shadowBlur={5}
+            onClick={handleClick}
+        />
+    );
 }
 
-class App extends Component {
-    render() {
-        return (
+function KonvaPage() {
+    return (
+        <div>
+            <h1>Konva Home</h1>
+            <div id="container"></div>
             <Stage width={window.innerWidth} height={window.innerHeight}>
                 <Layer>
                     <Text text="Try click on rect" />
                     <ColoredRect />
                 </Layer>
             </Stage>
-        );
-    }
+        </div>
+    )
 }
 
-render(<App />, document.getElementById("root"));
+export default KonvaPage;
