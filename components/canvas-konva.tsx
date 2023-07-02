50f74b98132be9dcc976d1a1963ad94561053614
Weng Jiahao <1010495847@qq.com>
2023-07-02 15:58:08 +0800

    feat: 序列化导入测试性能

--- components/canvas-konva.tsx
+++ components/canvas-konva.tsx
@@ -1,13 +1,17 @@
 'use client';
 import { Stage, Layer, Circle } from 'react-konva';
+import Konva from 'konva';
+import { useEffect, useRef } from 'react';
+import data from './konva-data.json'
 
 function Canvas(props) {
+    useEffect(() => {
+        console.log(data)
+        let stage = Konva.Node.create(JSON.stringify(data), 'container');
+    }, [])
+
     return (
-        <Stage width={window.innerWidth} height={window.innerHeight}>
-            <Layer>
-                <Circle x={200} y={100} radius={50} fill="green" />
-            </Layer>
-        </Stage>
+        <div id="container"></div>
     );
 }
 
