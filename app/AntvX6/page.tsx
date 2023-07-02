5ca1049cdfbe2f4df2f9101209916f65212bcbe5
Weng Jiahao <1010495847@qq.com>
2023-07-02 12:50:12 +0800

    feat: AntvX6测试数据更新

--- app/AntvX6/page.tsx
+++ app/AntvX6/page.tsx
@@ -13,28 +13,29 @@ function AntvX6HomePage() {
             container: document.getElementById('canvas'),
             width: 1000,
             height: 1000,
+            grid: true,
         })
 
-        const source = graph.addNode({
-            x: 300,
-            y: 40,
-            width: 80,
-            height: 40,
-            label: 'Hello',
-        })
-
-        const target = graph.addNode({
-            x: 420,
-            y: 180,
-            width: 80,
-            height: 40,
-            label: 'World',
-        })
-
-        graph.addEdge({source, target})
+        // const source = graph.addNode({
+        //     x: 300,
+        //     y: 40,
+        //     width: 80,
+        //     height: 40,
+        //     label: 'Hello',
+        // })
+        //
+        // const target = graph.addNode({
+        //     x: 420,
+        //     y: 180,
+        //     width: 80,
+        //     height: 40,
+        //     label: 'World',
+        // })
+        //
+        // graph.addEdge({source, target})
         graph.fromJSON(data)
         let time2 = +new Date()
-        console.log('exe time = ', time2 - time1)
+        console.log('exe. time = ', time2 - time1)
     }, [])
 
     return (
