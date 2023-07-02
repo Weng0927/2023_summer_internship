aac0ca592cbf96df5ddb3de9339fc055da50e4a1
Weng Jiahao <1010495847@qq.com>
2023-07-02 15:01:18 +0800

    fix: 采用动态引入方式，解决报错bug

--- app/konva.js/page.tsx
+++ app/konva.js/page.tsx
@@ -3,39 +3,13 @@ import React, { Component } from "react";
 import Konva from "konva";
 import { render } from "react-dom";
 import { Stage, Layer, Rect, Text } from "react-konva";
+import dynamic from 'next/dynamic'
 
-function ColoredRect() {
-    const [color, setColor] = React.useState("green");
-
-    const handleClick = () => {
-        setColor(Konva.Util.getRandomColor());
-    }
-
-    return (
-        <Rect
-            x={20}
-            y={20}
-            width={50}
-            height={50}
-            fill={color}
-            shadowBlur={5}
-            onClick={handleClick}
-        />
-    );
-}
+const Canvas = dynamic(() => import('@/components/canvas-konva'), { ssr: false })
 
 function KonvaPage() {
     return (
-        <div>
-            <h1>Konva Home</h1>
-            <div id="container"></div>
-            <Stage width={window.innerWidth} height={window.innerHeight}>
-                <Layer>
-                    <Text text="Try click on rect" />
-                    <ColoredRect />
-                </Layer>
-            </Stage>
-        </div>
+        <Canvas></Canvas>
     )
 }
 
